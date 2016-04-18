373b47e402884d255c6de10ac8824bc936b40df5
descent <descent0@gmail.com>
2016-04-18 10:18:07 +0800

    add SEP, MUL, DIV

--- mytype.h
+++ mytype.h
@@ -14,7 +14,7 @@ typedef unsigned int u32;
 
 typedef unsigned long long u64;
 
-enum ASTType {INVALID, ADD, MIN, NUMBER, LAST};
+enum ASTType {INVALID, SEP, ADD, MIN, MUL, DIV, NUMBER, LAST};
 
 
 #endif
