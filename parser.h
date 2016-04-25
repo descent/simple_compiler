63e4d4068ea043ebb4c919e4508a0da74b6db396
descent <descent0@gmail.com>
2016-04-25 13:43:16 +0800

    move parser.cpp some function prototype to parser.h

--- parser.h
+++ parser.h
@@ -1,8 +1,15 @@
 #ifndef PARSER_H
 #define PARSER_H
 
+#include "astnode.h"
+#include "op.h"
+
 ASTNode* expression();
 ASTNode* term();
 ASTNode* factor();
+ASTNode* expr();
+Precedence* next_op();
+ASTNode* do_shift(ASTNode* l, int prec);
+ASTNode* statement();
 
 #endif
