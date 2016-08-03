b1a8dba101242204bb1d9fe81b8725effdaaad66
descent <descent0@gmail.com>
2016-08-03 17:19:19 +0800

    change func_token asttype to FUNC_NAME

--- token.cpp
+++ token.cpp
@@ -10,7 +10,7 @@ Token else_block("else_block", ELSE_BLOCK);
 Token g_var_token("g_var", GLOBAL_VAR);
 Token var_token("var", VAR);
 Token para_token("para", VAR);
-Token func_token("func", NAME);
+Token func_token("func", FUNC_NAME);
 Token func_body_token("func_body", FUNC_BODY);
 Token func_call_token("func call", FUNC_CALL);
 Token prog_token("prog", PROG);
