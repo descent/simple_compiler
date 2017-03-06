9f5d78e33296be590818840d612333b28e3c5498
descent <descent0@gmail.com>
2017-03-06 22:01:13 +0800

    add branch and while test case files.

---
+++ test_pattern/b_l.c
@@ -0,0 +1,21 @@
+// loop and branch test
+
+int main()
+{
+  int i;
+  int j;
+
+  i=6;
+
+  if (i<6)
+  {
+    j=11;
+    printf("i<6, i: %d\n", i);
+  }
+  else
+  {
+    j=22;
+    printf("i>=6, i: %d\n", i);
+  }
+}
+
---
+++ test_pattern/w.c
@@ -0,0 +1,16 @@
+// while loop test
+
+int main()
+{
+  int i;
+  int j;
+
+  i=6;
+
+  while (i < 10)
+  {
+    i = i+1;
+    printf("i<6, i: %d\n", i);
+  }
+}
+
