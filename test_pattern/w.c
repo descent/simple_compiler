8b50e333f00a20aa875c0547e4899aa590ba9f49
descent <descent0@gmail.com>
2017-03-11 23:15:20 +0800

    add new test case

--- test_pattern/w.c
+++ test_pattern/w.c
@@ -7,10 +7,16 @@ int main()
 
   i=6;
 
-  while (i < 10)
+  //while (i < 10)
+  //while (i)
+  //while (5)
+  while (0)
+  //while (1+2)
+  //while (i*5)
+  //while (i*5 < 100)
   {
     i = i+1;
-    printf("i<6, i: %d\n", i);
+    printf("i: %d\n", i);
   }
 }
 
