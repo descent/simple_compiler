5135c11306a03edde91e66842b83043aa79d4c2f
descent <descent0@gmail.com>
2017-03-21 22:58:55 +0800

    para_token set to PARA ast_type

--- token.cpp
+++ token.cpp
@@ -9,7 +9,7 @@ Token then_block("then_block", THEN_BLOCK);
 Token else_block("else_block", ELSE_BLOCK);
 Token g_var_token("g_var", GLOBAL_VAR);
 Token var_token("var", VAR);
-Token para_token("para", VAR);
+Token para_token("para", PARA);
 Token func_token("func", FUNC_NAME);
 Token func_body_token("func_body", FUNC_BODY);
 Token func_call_token("func call", FUNC_CALL);
